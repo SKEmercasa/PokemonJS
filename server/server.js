7f0247227aa23dd79df12e04591efe06bb8eb958
Sergey Kiloshenko <sergeykiloshenko@list.ru>
2021-12-26 09:27:07 +0300

    second commit

--- server/server.js
+++ server/server.js
@@ -9,6 +9,8 @@ const express = require('express'),
 
     https = require('https').Server(options, app);
 
+app.use("/", express.static("d:/Software/PokemonJS/public/Reg"));
+
 https.listen(5443, "192.168.1.100", () => {
     console.log("Server running...")
 });
